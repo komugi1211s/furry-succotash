d2134d73abc2972e0b56504981bd51ee49370a1a
fuzzyperson@home <70929713+komugi1211s@users.noreply.github.com>
2021-10-09 15:16:11 +1000

    resulting change

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 (wheel reinvent jam participant)
 
 Basic File watcher that runs specified command each time when there's a change in something.
+
+## Usage
+
+*Windows*
+requires MSVC.
+modify `build.bat` for appropriate SDL / GLFW path and run `build.bat`.
+
+```
+./build.bat
+```
+
+run `/dist/main.exe`, specify appropriate directory to watch for / process to run.
+it fires the specified command whenever detects new file creation / modification. (_TODO: deletion_)
+
+
+*Unix*
+_TODO_
--- build.bat
+++ build.bat
@@ -3,9 +3,21 @@
 if not exist dist mkdir dist
 pushd dist
 
-cl.exe /Zi /c /Fo"./microui.obj" ../src/vendor/microui.c 
-cl.exe /Zi /I "W:\CppProject\CppLib\SDL2\include" /I "W:\CppProject\CppLib\GL\include" /c /Fo"./template_sdl_microui_opengl3.obj" ../src/template_sdl_microui_opengl3.c
+set SDL_INCLUDE_PATH="W:\CppProject\CppLib\SDL2\include"
+set SDL_LIB_PATH="W:\CppProject\CppLib\SDL2\lib\x64"
 
-cl.exe /Zi /nologo /I "W:\CppProject\CppLib\SDL2\include" /I "W:\CppProject\CppLib\GL\include" ../src/main.cpp microui.obj ./template_sdl_microui_opengl3.obj /link /LIBPATH:"W:\CppProject\CppLib\SDL2\lib\x64" /LIBPATH:"W:\CppProject\CppLib\GL\lib" user32.lib shell32.lib Comdlg32.lib opengl32.lib SDL2.lib
+REM ==============================
+REM compiling dependency as C
+REM ==============================
+cl.exe /Zi /nologo                      /c /Fo"./microui.obj"                      ../src/vendor/microui.c 
+cl.exe /Zi /nologo /I%SDL_INCLUDE_PATH% /c /Fo"./template_sdl_microui_opengl3.obj" ../src/template_sdl_microui_opengl3.c
 
+REM ==============================
+REM compiling main file as C++
+REM ==============================
+
+set FILES=../src/main.cpp microui.obj template_sdl_microui_opengl3.obj
+set LIBS=user32.lib shell32.lib Comdlg32.lib opengl32.lib SDL2.lib
+
+cl.exe /Zi /nologo /I %SDL_INCLUDE_PATH% %FILES% /link /LIBPATH:%SDL_LIB_PATH% %LIBS%
 popd
